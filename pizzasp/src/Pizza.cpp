f3c8cab5c1d0ffd1a63a489843f156ffee6bf305
PaSawi <sawicki.patryk@gmail.com>
2023-03-24 14:07:55 +0100

    pizzasp

---
+++ pizzasp/src/Pizza.cpp
@@ -0,0 +1,23 @@
+#include "Pizza.hpp"
+
+Pizza::Pizza(std::string const & name, double price, minutes bakingTime)
+    : name_(name)
+    , price_(price)
+    , bakingTime_(bakingTime)
+{}
+
+std::string Pizza::getName() const
+{
+    return name_;
+}
+
+double Pizza::getPrice() const
+{
+    return 30.0;
+}
+
+minutes Pizza::getBakingTime() const
+{
+    return bakingTime_;
+}
+
